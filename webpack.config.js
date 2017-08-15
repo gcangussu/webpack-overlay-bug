92bca5d0e274724184106a96ae1281e1e5f08d89
Gabriel Cangussu <gabrielcangussu@gmail.com>
2017-08-14 23:37:49 -0300

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [path.join(__dirname, 'index.js')],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Overlay Bug'
+    })
+  ],
+  devServer: {
+    contentBase: false,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    publicPath: '/',
+    https: true,
+    hot: true,
+    hotOnly: true
+  },
+  output: {
+    filename: '[name].js',
+    path: '/'
+  },
+  performance: {
+    hints: "warning"
+  }  
+}
